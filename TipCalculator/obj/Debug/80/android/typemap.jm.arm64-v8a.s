380a4efbff41464003aa66fd950b9a76d0835d00
HenryH <52370186+Zikun-Huang@users.noreply.github.com>
2019-08-02 13:35:24 +0800

    first commit

---
+++ TipCalculator/obj/Debug/80/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 0f457a4434d086b8fd7f8bf730310a3c2786f466 */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	988
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 258857
+	.include	"typemap.jm.inc"
---
+++ TipCalculator/obj/Debug/80/android/typemap.mj.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: dfd92be83c849176284a91a0ac6b5d155eafd957 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1101
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 288463
+	.include	"typemap.mj.inc"
